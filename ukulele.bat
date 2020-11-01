6e5057637f63e930ae7f0d841255199eb58e6cc6
OtterBoops <me44888@gmail.com>
2020-11-01 16:38:45 +0200

    Add windows ukulele script

---
+++ ukulele.bat
@@ -0,0 +1,5 @@
+@echo off
+::Use the -d flag to run the bot in detached mode.
+
+call gradlew.bat build
+if "%1"=="-d" (start javaw -jar "build\libs\ukulele.jar") else (java -jar "build\libs\ukulele.jar")
