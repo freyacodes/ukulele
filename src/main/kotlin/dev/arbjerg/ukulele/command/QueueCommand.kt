0ca49f1cd315388e6c9b606f47f7693e50dc7700
OtterBoops <me44888@gmail.com>
2020-11-03 13:47:48 +0200

    Remove unnecessary exports

--- src/main/kotlin/dev/arbjerg/ukulele/command/QueueCommand.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/command/QueueCommand.kt
@@ -1,15 +1,9 @@
 package dev.arbjerg.ukulele.command
 
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
-import dev.arbjerg.ukulele.audio.Player
 import dev.arbjerg.ukulele.audio.PlayerRegistry
 import dev.arbjerg.ukulele.jda.Command
 import dev.arbjerg.ukulele.jda.CommandContext
-import net.dv8tion.jda.api.Permission
 import org.springframework.stereotype.Component
 
 @Component
