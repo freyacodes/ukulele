d9075b38e85d05a1d693d868b256cee9bd1ebcff
OtterBoops <me44888@gmail.com>
2020-11-04 17:59:57 +0200

    Add some basic formatting and total queue length

--- src/main/kotlin/dev/arbjerg/ukulele/command/QueueCommand.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/command/QueueCommand.kt
@@ -5,6 +5,7 @@ import dev.arbjerg.ukulele.audio.PlayerRegistry
 import dev.arbjerg.ukulele.jda.Command
 import dev.arbjerg.ukulele.jda.CommandContext
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class QueueCommand (
@@ -17,13 +18,25 @@ class QueueCommand (
 
     private fun CommandContext.printQueue(queue: List<AudioTrack>): String {
         val sb = StringBuilder()
+        var totalDuration: Long = 0
         if (queue.isEmpty())
-            return "Empty Queue"
+            return "Not currently playing anything."
 
         queue.forEachIndexed { index, t -> 
-            sb.append("`[${index+1}]` ${t.info.title} \n")
+            sb.append("`[${index+1}]` **${t.info.title}** `[${humanReadableTime(t.duration)}]`\n")
+            totalDuration += t.info.length
         }
 
+        sb.append("\n There are **${queue.size}** tracks with a remaining length of **${humanReadableTime(totalDuration)}** in the queue.")
+
         return sb.toString()
+    }
+
+    private fun CommandContext.humanReadableTime(length: Long): String {
+        var hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(length),
+        TimeUnit.MILLISECONDS.toMinutes(length) % TimeUnit.HOURS.toMinutes(1),
+        TimeUnit.MILLISECONDS.toSeconds(length) % TimeUnit.MINUTES.toSeconds(1));
+
+        return hms
     }
 }
