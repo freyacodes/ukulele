aed1a6a9c7d57aa5c1e15dec3b2be2ad430434d4
Frederik Ar. Mikkelsen <fred@frederikam.com>
2020-11-10 23:16:15 +0100

    Disable gradle daemon (batch)

--- ukulele.bat
+++ ukulele.bat
@@ -1,5 +1,5 @@
 @echo off
 ::Use the -d flag to run the bot in detached mode.
 
-call gradlew.bat build
+call gradlew.bat --no-daemon build
 if "%1"=="-d" (start javaw -jar "build\libs\ukulele.jar") else (java -jar "build\libs\ukulele.jar")
