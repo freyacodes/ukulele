ed72d95169291463d495df5f1fc429c9ffc2d764
Axepoi <axepoi@hotmail.com>
2021-09-27 19:23:50 -0500

    Create LoopCommand.kt

---
+++ src/main/kotlin/dev/arbjerg/ukulele/command/LoopCommand.kt
@@ -0,0 +1,20 @@
+package dev.arbjerg.ukulele.command
+
+import dev.arbjerg.ukulele.features.HelpContext
+import dev.arbjerg.ukulele.jda.Command
+import dev.arbjerg.ukulele.jda.CommandContext
+import org.springframework.stereotype.Component
+
+@Component
+class LoopCommand : Command ("loop", "l") {
+    override suspend fun CommandContext.invoke() {
+        player.loop()
+        if (player.isPaused) {reply("Looping is now enabled.")}
+        else{reply("Looping is now disabled.")}
+    }
+
+    override fun HelpContext.provideHelp() {
+        addUsage("")
+        addDescription("Toggles the playback looping.")
+    }
+}
