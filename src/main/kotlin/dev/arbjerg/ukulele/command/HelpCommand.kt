8878de8d1966b60527509bded6f1b7da9efcbcb7
Peter Fabinski <github@peterfab.com>
2021-10-18 17:08:14 -0400

    Add basic command list to help (#23)
    * Add basic command list to help
    
    * Add newline after list
    
    * Update help message for help
    
    * Improve style for help command
    
    * Additional style changes
    
    * Remove unneeded new dependency

--- src/main/kotlin/dev/arbjerg/ukulele/command/HelpCommand.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/command/HelpCommand.kt
@@ -3,18 +3,30 @@ package dev.arbjerg.ukulele.command
 import dev.arbjerg.ukulele.features.HelpContext
 import dev.arbjerg.ukulele.jda.Command
 import dev.arbjerg.ukulele.jda.CommandContext
+import net.dv8tion.jda.api.MessageBuilder
 import org.springframework.stereotype.Component
 
 @Component
 class HelpCommand : Command("help") {
     override suspend fun CommandContext.invoke() {
-        val command = beans.commandManager[argumentText.trim()] ?: return replyHelp()
-        replyHelp(command)
+        if (argumentText.isNotBlank()) {
+            replyHelp(beans.commandManager[argumentText.trim()] ?: command)
+        } else {
+            val msg = MessageBuilder()
+                .append("Available commands:")
+                .appendCodeBlock(buildString {
+                    beans.commandManager.getCommands().forEach {
+                        appendLine((listOf(it.name) + it.aliases).joinToString())
+                    }
+                }, "")
+                .append("\nUse `${trigger} <command>` to see more details.")
+            replyMsg(msg.build())
+        }
     }
 
     override fun HelpContext.provideHelp() {
         addUsage("")
-        addDescription("Displays general help. (unfinished)") // TODO
+        addDescription("Displays a list of commands and aliases.")
         addUsage("<command>")
         addDescription("Displays help about a specific command.")
     }
