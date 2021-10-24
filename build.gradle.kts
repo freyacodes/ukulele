c3ad986d20433596d9cc6e93039e3a3a7139ba55
Freya Arbjerg <freya@arbjerg.dev>
2021-10-24 20:57:43 +0200

    Remove old lavaplayer

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:4.3.0_277")
-    implementation("com.sedmelluq:lavaplayer:1.3.78")
+    //implementation("com.sedmelluq:lavaplayer:1.3.78")
     implementation("com.github.walkyst:lavaplayer-fork:1.3.96")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
