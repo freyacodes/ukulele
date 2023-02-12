a8c0d2256f480625e431afeb49daf0b8db13bb5a
JustinFreitas <justin.s.freitas@me.com>
2023-02-12 15:01:36 -0700

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:4.4.0_350")
+    implementation("net.dv8tion:JDA:4.4.0_352")
     implementation("com.github.walkyst:lavaplayer-fork:1.3.99.2")
     implementation("com.github.aikaterna:lavaplayer-natives:f067f4dae7")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
@@ -28,7 +28,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("io.r2dbc:r2dbc-h2")
     implementation("org.flywaydb:flyway-core")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.2")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.3")
 
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
