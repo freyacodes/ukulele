06949fae49a687087ae7ab082961734e7d6982ba
JustinFreitas <justin.s.freitas@me.com>
2023-03-26 10:39:35 -0600

    Dep update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "2.4.13"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
 }
