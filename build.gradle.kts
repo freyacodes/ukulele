8cdf1b8f846ec96f24ed2454d50fed9e7d265ebf
JustinFreitas <justin.s.freitas@me.com>
2023-03-26 19:48:31 -0600

    Kotlin 1.5.32 and Gradle 6.9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "2.4.13"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
+    kotlin("jvm") version "1.5.32"
+    kotlin("plugin.spring") version "1.5.32"
 }
 
 group = "dev.arbjerg"
