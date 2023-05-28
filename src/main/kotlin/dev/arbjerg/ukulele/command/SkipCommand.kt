885445389cebed446544fc9a45d5c2a0c950bd95
JustinFreitas <justin.s.freitas@me.com>
2023-05-28 17:19:45 -0600

    Brought back skipRange() with fixes to it, particularly around top boundary.

--- src/main/kotlin/dev/arbjerg/ukulele/command/SkipCommand.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/command/SkipCommand.kt
@@ -12,7 +12,8 @@ class SkipCommand : Command("skip", "s") {
         val args = argumentText.split("\\s+".toRegex())
         when {
             args.isEmpty() || args[0].isEmpty() -> skipNext()
-            else -> skipIndex(args.last().toInt())
+            args.size == 1 -> skipIndex(args[0].toInt())
+            else -> skipRange()
         }
     }
 
@@ -25,9 +26,26 @@ class SkipCommand : Command("skip", "s") {
         if (ind == 0) {
             player.seek(0)
             reply("Skipping to current track (restarting)")
+        } else if (ind > player.tracks.size - 1) {
+            player.stop()
+            reply("Skipping past end of queue, player stopped.")
         } else {
-            val endRange = ind - 1
+            val endRange = (ind - 1).coerceAtLeast(0)
             printSkipped(player.skip(0..endRange))
+        }
+    }
+
+    private fun CommandContext.skipRange() {
+        val args = argumentText.split("\\s+".toRegex())
+
+        val n1 = (args[0].toInt() - 1).coerceAtLeast(0)
+        var n2 = args[1].toInt()
+        if (n2 > player.tracks.size) {
+            player.stop()
+            reply("Skipping past end of queue, player stopped.")
+        } else {
+            n2 = (n2 - 1).coerceAtLeast(0)
+            printSkipped(player.skip(n1..n2))
         }
     }
 
